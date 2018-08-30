5186f7e3ca101a846eec9f132243a98a37ebdfea
NavyAdmiral <keccaksha3@gmail.com>
2018-08-29 21:46:03 -0300

    wip

--- gui/src/containers/JobSpec.js
+++ gui/src/containers/JobSpec.js
@@ -8,21 +8,18 @@ import Breadcrumb from 'components/Breadcrumb'
 import BreadcrumbItem from 'components/BreadcrumbItem'
 import Card from '@material-ui/core/Card'
 import JobRunsList from 'components/JobRunsList'
-import formatInitiators from 'utils/formatInitiators'
+import { isWebInitiator, formatInitiators } from 'utils/jobSpecInitiators'
 import jobSpecDefinition from 'utils/jobSpecDefinition'
 import Link from 'components/Link'
 import CopyJobSpec from 'components/CopyJobSpec'
 import matchRouteAndMapDispatchToProps from 'utils/matchRouteAndMapDispatchToProps'
 import { withStyles } from '@material-ui/core/styles'
 import { connect } from 'react-redux'
-import { fetchJobSpec } from 'actions'
-import {
-  jobSpecSelector,
-  jobRunsSelector,
-  jobRunsCountSelector
-} from 'selectors'
+import { fetchJobSpec, submitJobSpecRun } from 'actions'
+import { jobSpecSelector, jobRunsSelector, jobRunsCountSelector } from 'selectors'
 import { LATEST_JOB_RUNS_COUNT } from 'connectors/redux/reducers/jobRuns'
-import { Divider } from '@material-ui/core'
+import { Divider, Button } from '@material-ui/core'
+import { BridgeAndJobNotifications } from 'components/FormNotifications'
 
 const styles = theme => ({
   title: {
@@ -49,67 +46,97 @@ const styles = theme => ({
     marginTop: theme.spacing.unit * 3,
     marginLeft: theme.spacing.unit * 3,
     display: 'block'
+  },
+  flash: {
+    textAlign: 'center',
+    paddingTop: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit
   }
 })
 
-const renderJobSpec = ({classes, jobSpec, jobRunsCount}) => (
-  <Grid container spacing={40}>
-    <Grid item xs={8}>
-      <PaddedCard>
-        <Grid container alignItems='flex-start'>
-          <Grid item xs={6}>
-            <Typography variant='title' className={classes.definitionTitle}>
-              Definition
-            </Typography>
+const renderJobSpec = ({ classes, jobSpec, jobRunsCount, success, submitJobSpecRun }) => {
+  const handleClick = () => { submitJobSpecRun(jobSpec.id) }
+
+  return (
+    <Grid container spacing={40}>
+      <Grid item xs={8}>
+        <PaddedCard>
+          <BridgeAndJobNotifications
+            success={success}
+            error={{}}
+            authenticated
+            classes={classes}
+          />
+          <Grid container alignItems='baseline'>
+            <Grid item xs={8}>
+              <Typography variant='title' className={classes.definitionTitle}>
+                Definition
+              </Typography>
+            </Grid>
+            <Grid item>
+              {isWebInitiator(jobSpec.initiators) && (
+                <Button variant='outlined' color='primary' onClick={handleClick}>
+                  Run
+                </Button>
+              )}
+            </Grid>
+            <Grid item>
+              <CopyJobSpec JobSpec={jobSpecDefinition(jobSpec)} />
+            </Grid>
+            <Grid item xs={12}>
+              <Divider light className={classes.divider} />
+            </Grid>
+            <PrettyJson object={jobSpecDefinition(jobSpec)} />
           </Grid>
-          <Grid item xs={6} align='right'>
-            <CopyJobSpec JobSpec={jobSpecDefinition(jobSpec)} />
-          </Grid>
-          <Grid item xs={12}>
-            <Divider light className={classes.divider} />
-          </Grid>
-          <PrettyJson object={jobSpecDefinition(jobSpec)} />
-        </Grid>
-      </PaddedCard>
-    </Grid>
-    <Grid item xs={4}>
-      <PaddedCard>
-        <Grid container spacing={16}>
-          <Grid item xs={12}>
-            <Typography variant='subheading' color='textSecondary'>ID</Typography>
-            <Typography variant='body1' color='inherit'>
-              {jobSpec.id}
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography variant='subheading' color='textSecondary'>Created</Typography>
-            <Typography variant='body1' color='inherit'>
-              {jobSpec.createdAt}
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Grid container spacing={16}>
-              <Grid item xs={6}>
-                <Typography variant='subheading' color='textSecondary'>Initiator</Typography>
-                <Typography variant='body1' color='inherit'>
-                  {formatInitiators(jobSpec.initiators)}
-                </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography variant='subheading' color='textSecondary'>Run Count</Typography>
-                <Typography variant='body1' color='inherit'>
-                  {jobRunsCount}
-                </Typography>
+        </PaddedCard>
+      </Grid>
+      <Grid item xs={4}>
+        <PaddedCard>
+          <Grid container spacing={16}>
+            <Grid item xs={12}>
+              <Typography variant='subheading' color='textSecondary'>
+                ID
+              </Typography>
+              <Typography variant='body1' color='inherit'>
+                {jobSpec.id}
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant='subheading' color='textSecondary'>
+                Created
+              </Typography>
+              <Typography variant='body1' color='inherit'>
+                {jobSpec.createdAt}
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Grid container spacing={16}>
+                <Grid item xs={6}>
+                  <Typography variant='subheading' color='textSecondary'>
+                    Initiator
+                  </Typography>
+                  <Typography variant='body1' color='inherit'>
+                    {formatInitiators(jobSpec.initiators)}
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography variant='subheading' color='textSecondary'>
+                    Run Count
+                  </Typography>
+                  <Typography variant='body1' color='inherit'>
+                    {jobRunsCount}
+                  </Typography>
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
-        </Grid>
-      </PaddedCard>
+        </PaddedCard>
+      </Grid>
     </Grid>
-  </Grid>
-)
+  )
+}
 
-const renderLatestRuns = ({jobSpecId, classes, latestJobRuns, jobRunsCount}) => (
+const renderLatestRuns = ({ jobSpecId, classes, latestJobRuns, jobRunsCount }) => (
   <React.Fragment>
     <Typography variant='title' className={classes.lastRun}>
       Last Run
@@ -118,19 +145,17 @@ const renderLatestRuns = ({jobSpecId, classes, latestJobRuns, jobRunsCount}) => 
     <Card>
       <JobRunsList jobSpecId={jobSpecId} runs={latestJobRuns} />
     </Card>
-    {jobRunsCount > LATEST_JOB_RUNS_COUNT &&
+    {jobRunsCount > LATEST_JOB_RUNS_COUNT && (
       <Link to={`/job_specs/${jobSpecId}/runs`} className={classes.showMore}>
         Show More
       </Link>
-    }
+    )}
   </React.Fragment>
 )
 
-const renderFetching = () => (
-  <div>Fetching...</div>
-)
+const renderFetching = () => <div>Fetching...</div>
 
-const renderDetails = (props) => {
+const renderDetails = props => {
   if (props.jobSpec) {
     return (
       <React.Fragment>
@@ -149,7 +174,7 @@ export class JobSpec extends Component {
   }
 
   render () {
-    const {classes, jobSpecId} = this.props
+    const { classes, jobSpecId } = this.props
 
     return (
       <div>
@@ -184,18 +209,20 @@ const mapStateToProps = (state, ownProps) => {
   const jobSpec = jobSpecSelector(state, jobSpecId)
   const jobRunsCount = jobRunsCountSelector(state, jobSpecId)
   const latestJobRuns = jobRunsSelector(state)
+  const success = state.create.successMessage
 
   return {
     jobSpecId,
     jobSpec,
     latestJobRuns,
-    jobRunsCount
+    jobRunsCount,
+    success
   }
 }
 
 export const ConnectedJobSpec = connect(
   mapStateToProps,
-  matchRouteAndMapDispatchToProps({fetchJobSpec})
+  matchRouteAndMapDispatchToProps({ fetchJobSpec, submitJobSpecRun })
 )(JobSpec)
 
 export default withStyles(styles)(ConnectedJobSpec)
